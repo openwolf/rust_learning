9a5f22a9efc1d4fb001d807020de5b8f0306325d
openwolf <200550821@qq.com>
2018-02-23 23:35:36 +0800

    提交 cargo 创建的项目

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-eb0b735cbe71a438.d
@@ -0,0 +1,5 @@
+/home/openwolf/learn/rust_learning/projects/hello_cargo/target/debug/deps/hello_cargo-eb0b735cbe71a438: src/main.rs
+
+/home/openwolf/learn/rust_learning/projects/hello_cargo/target/debug/deps/hello_cargo-eb0b735cbe71a438.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/openwolf/learn/rust_learning/projects/hello_cargo/target/debug/hello_cargo: /home/openwolf/learn/rust_learning/projects/hello_cargo/src/main.rs
