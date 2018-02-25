a0b0d9c0925d869e0bf29f69b303b2c80af5fe4f
openwolf <200550821@qq.com>
2018-02-25 12:52:20 +0800

    新增 guessing_game 项目

---
+++ projects/guessing_game/target/debug/deps/guessing_game-ffafbb6fe5f64b69.d
@@ -0,0 +1,5 @@
+/home/openwolf/learn/rust_learning/projects/guessing_game/target/debug/deps/guessing_game-ffafbb6fe5f64b69: src/main.rs
+
+/home/openwolf/learn/rust_learning/projects/guessing_game/target/debug/deps/guessing_game-ffafbb6fe5f64b69.d: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/openwolf/learn/rust_learning/projects/guessing_game/target/debug/guessing_game: /home/openwolf/learn/rust_learning/projects/guessing_game/src/main.rs
